6fb48becb522da7f8352abaafbba79b9e213527f
Ryan Reynolds <ryanrey@uga.edu>
2015-04-29 00:03:36 -0400

    List Works
    List now has a dropdown on the  window load function. The results area
    also goes from unhidden to shown when search button is clicked.

--- babynames.php
+++ babynames.php
@@ -1,1 +1,25 @@
+<?php
 
+$qType = isset($_GET['q']) ? $_GET['q'] : '';
+/*
+$qType = $_REQUEST['q'];
+
+*/
+//$qType = strtolower($qType);
+
+ echo $qType;
+
+if ($qType == "list"){
+	$listFile = fopen("list.txt","r+") or die ("Unable to open file!");
+	while(!feof($listFile)){
+		echo fgets($listFile);
+	}
+
+fclose($listFile);
+
+}
+else{
+	echo "fuck";
+}
+
+?>
--- names.js
+++ names.js
@@ -1,1 +1,37 @@
 
+window.onload = function(){
+	new Ajax.Request(
+		"babynames.php", 
+		{
+		method: "get",
+		parameters: "q=list",
+		onSuccess: loadNames
+		
+	}
+	);
+
+	$("search").onclick = searchClicked;
+}
+
+function searchClicked(){
+	$("resultsarea").show();
+}
+function getNames(){
+
+	
+}
+function loadNames(ajax){
+	var list = ajax.responseText.split("\n");
+	var dropList = $("allnames");
+
+	for(var i = 0; i < list.length; i++){
+	  var dropItem = document.createElement("option");
+	  dropItem.innerHTML = list[i];
+	  dropList.appendChild(dropItem);	
+	}
+
+	dropList.disabled= false;
+
+
+
+}
